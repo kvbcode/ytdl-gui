1a187828fa8ad4e51e9d7c1d9ce8004be26bc4fc
Cyber <cybermanic@yandex.ru>
2021-10-11 17:13:21 +0500

    v0.3.5-SNAPSHOT

--- build-bundle-with-jre.cmd
+++ build-bundle-with-jre.cmd
@@ -1,4 +1,4 @@
-set APP_JAR_NAME=ytdl-gui-0.3-SNAPSHOT.jar
+set APP_JAR_NAME=ytdl-gui-0.3.5-SNAPSHOT.jar
 
 "%JAVA_HOME%\bin\jlink" --strip-debug --compress=2 --module-path "%JAVA_HOME%\jmods" --add-modules java.base,java.desktop --output "bundle\jre"
 copy target\%APP_JAR_NAME% bundle\
